ba123fd8caf40735d009cd7237c692a3cc0bef40
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-02 02:53:46 -0700

    All Clear

--- src/App.js
+++ src/App.js
@@ -4,51 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Muzammil
-        </a>
-      </header> */}
-      <p>
-        Muzammil Husnain
-      </p>
-    <table>
-    <tr>
-                <td>
-                    <ul>
-                        <li>Ara</li>
-                        <li>profile</li>
-                        <li>Sheet</li>
-                        <li>170*</li>
-                    </ul>
-                </td>
-                <td>
-                    <ul>
-                        <li>Ara</li>
-                        <li>profile</li>
-                        <li>Sheet</li>
-                        <li>170*</li>
-                    </ul>
-                </td>
-                <td>
-                    <ol>
-                        <li>Ara</li>
-                        <li>profile</li>
-                        <li>Sheet</li>
-                        <li>170*</li>
-                    </ol>
-                </td>
-            </tr>
-    </table>
 
     </div>
   );
